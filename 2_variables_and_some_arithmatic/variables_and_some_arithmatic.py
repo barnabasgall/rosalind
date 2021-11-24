c0bff9be8f765089e2226a2c1111bd25730eb9b0
barnabasg <barnabasgall@protonmail.com>
2021-11-24 12:34:35 +1100

    first problem

---
+++ 2_variables_and_some_arithmatic/variables_and_some_arithmatic.py
@@ -0,0 +1,6 @@
+# Problem: 
+# Given: two positive integers a and b, each less than 1000.
+# Return: The integer corresponding to the sqyare of the hypotenuse of the right angle whose legs have lengths a and b.
+a = 886
+b = 904
+print( a**2 + b**2)
